62f19de0037fbccb07eba8d70bd4b1a5cc85a63f
Martin Charles <martincharles07@gmail.com>
2026-03-15 00:48:07 -0500

    fix(nix): make nix run build reliably on macOS (#554)

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
           version = packageJson.version;
           src = ./.;
           nodejs = pkgs.nodejs_20;
-          npmDepsHash = "sha256-GWBewwZD6Q3wstZCvVYToiRHRPvrVQcPBJGJPvook6o=";
+          npmDepsHash = "sha256-YbHVvsYijeCw0FlTWx4yhvfNH+yHRAm7fcfYKG/SkU0=";
           npmBuildScript = "build";
           installPhase = ''
             mkdir -p $out
@@ -58,9 +58,10 @@
           nativeBuildInputs = [
             pkgs.cargo-tauri
             pkgs.cmake
+            pkgs.llvmPackages.libclang
+            pkgs.perl
             pkgs.pkg-config
           ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
-            pkgs.llvmPackages.libclang
             pkgs.wrapGAppsHook3
           ];
 
@@ -70,8 +71,7 @@
 
           TAURI_CONFIG = tauriConfig;
 
-          LIBCLANG_PATH = pkgs.lib.optionalString pkgs.stdenv.isLinux
-            "${pkgs.llvmPackages.libclang.lib}/lib";
+          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
 
           preBuild = ''
             mkdir -p dist
